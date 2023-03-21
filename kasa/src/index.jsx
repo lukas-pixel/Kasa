c29200848e4546a6574b6aa1341f4c99634559be
marine cherdo <marine.cherdo13@gmail.com>
2023-03-21 16:42:34 +0100

    Création du header et correction du probleme de router, routes et route

--- kasa/src/index.jsx
+++ kasa/src/index.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/index'
-import FicheLogement from "./pages/Fiche-Logement";
+import Propos from "./pages/Propos";
+import Header from "./components/Header"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router exact path="/">
-      <Home />
-    </Router>
-    <Router path="/fichelogement">
-      <FicheLogement />
+    <Router>
+      <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/propos" element={<Propos />} />
+        </Routes>
     </Router>
   </React.StrictMode>
 )

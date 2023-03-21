18fd0e699c806907eaf563ddf6fa869e061f1e2d
marine cherdo <marine.cherdo13@gmail.com>
2023-03-21 16:50:21 +0100

    mise en palce de la page d'erreur

--- kasa/src/index.jsx
+++ kasa/src/index.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/index'
 import Propos from "./pages/Propos";
 import Header from "./components/Header"
+import Error from "./components/Error"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,6 +14,7 @@ root.render(
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/propos" element={<Propos />} />
+          <Route element={<Error />} />
         </Routes>
     </Router>
   </React.StrictMode>

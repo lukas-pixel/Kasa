3bac1b4257c2448bb29e5cb0ddf3a771ee0a2905
marine cherdo <marine.cherdo13@gmail.com>
2023-03-24 14:16:41 +0100

    Début du style dans le Header
    - couleur pour le Accueil et le propos 
    - mise en place de taille

--- kasa/src/components/Header/index.jsx
+++ kasa/src/components/Header/index.jsx
@@ -1,10 +1,21 @@
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import colors from '../../style/colors'
+
+const StyledLink = styled(Link)`
+    padding: 10px 15px;
+    text-decoration: none;
+    color: ${colors.primary};
+    text-align: center;
+    Font-family: 'Montserrat';
+    font-size: 24px;
+`
 
 function Header() {
     return (
         <nav>
-            <Link to="/">Accueil</Link>
-            <Link to="/propos">A Propos</Link>
+            <StyledLink to="/">Accueil</StyledLink>
+            <StyledLink to="/propos">A Propos</StyledLink>
         </nav>
     )
 }

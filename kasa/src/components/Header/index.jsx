c25b8ff8c1aa6f5a367283d2aace0e51b05fb2fb
marine cherdo <marine.cherdo13@gmail.com>
2023-03-25 09:31:16 +0100

    Header
    - mise en place du logo
    - mise en couleur de la typo et premiere mise en forme

--- kasa/src/components/Header/index.jsx
+++ kasa/src/components/Header/index.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import colors from '../../style/colors'
+import logo from '../../assets/LogoHeader.png'
 
 const StyledLink = styled(Link)`
     padding: 10px 15px;
@@ -11,12 +12,24 @@ const StyledLink = styled(Link)`
     font-size: 24px;
 `
 
+const HomeLogo = styled.img`
+    height: 70px;
+`
+
+const NavContainer = styled.nav`
+  padding: 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
 function Header() {
     return (
-        <nav>
+        <NavContainer>
+            <HomeLogo src={logo} alt="Logo Kasa" />
             <StyledLink to="/">Accueil</StyledLink>
             <StyledLink to="/propos">A Propos</StyledLink>
-        </nav>
+        </NavContainer>
     )
 }
 

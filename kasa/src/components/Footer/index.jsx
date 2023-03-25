4b64887d05b78e3d7cae6b5e58cdfe2e7b3d1c62
marine cherdo <marine.cherdo13@gmail.com>
2023-03-25 10:07:46 +0100

    mise en place du footer
    - Mise en couleur du footer
    - mise en place des éléments du footer

--- kasa/src/components/Footer/index.jsx
+++ kasa/src/components/Footer/index.jsx
@@ -1,8 +1,28 @@
+import styled from 'styled-components'
+import logoFooter from "../../assets/LogoFooter.png"
+import colors from '../../style/colors'
+
+const FooterColor = styled.div`
+    background-color: ${colors.tertiary};
+    display: flex;
+    align-content: center;
+    justify-content: center;
+`
+
+const FooterImg = styled.img`
+    height: 40px;
+`
+
+const FooterText = styled.h2`
+    color: ${colors.secondary};
+`
+
 function Footer() {
     return (
-        <div>
-            <h1>Footer</h1>
-        </div>
+        <FooterColor>
+            <FooterImg src={logoFooter} alt="logo Kasa" />
+            <FooterText><br />© 2020 Kasa. All rights reserved</FooterText>
+        </FooterColor>
     )
 }
 

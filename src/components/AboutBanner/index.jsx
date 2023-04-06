701fda4b6fb13304b946a4fe1aa36f183ef752d4
marine cherdo <marine.cherdo13@gmail.com>
2023-04-06 11:32:20 +0200

    ajout d'un fond noir sur l'image de fond

--- src/components/AboutBanner/index.jsx
+++ src/components/AboutBanner/index.jsx
@@ -4,17 +4,25 @@ import PaysageImg from '../../assets/kalen-emsley.png'
 
 const ContainerImg = styled.div`
     height: 223px;
-    background-image: url("${PaysageImg}");
-    background-position: center;
-    background-size: cover;
     background-color: ${colors.Black};
     border-radius: 25px;
     margin: 0 50px 0 50px;
 `
 
+const AboutImg = styled.div`
+    height: 223px;
+    background-image: url("${PaysageImg}");
+    background-position: center;
+    background-size: cover;
+    border-radius: 25px;
+    opacity: 0.6;
+`
+
 function AboutBanner() {
     return (
-        <ContainerImg></ContainerImg>
+        <ContainerImg>
+            <AboutImg></AboutImg>
+        </ContainerImg>
     )
 }
 

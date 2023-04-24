fe02610a2803297e85fd85129f85b6f1ccf25d72
marine cherdo <marine.cherdo13@gmail.com>
2023-04-24 09:39:58 +0200

    Ajout du media query : réajustement du texte

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,28 +1,10 @@
-import styled from 'styled-components'
-import colors from '../../style/colors'
-
-const ContainerBanner = styled.div`
-    position: relative;
-    height: 223px;
-    border-radius: 25px;
-    background-position: center;
-    background-size: cover;
-    text-align: center;
-`
-
-const TextBanner = styled.h1`
-    position: absolute;
-    top: 30%;
-    left: 30%;
-    color: ${colors.White};
-    text-align: center;
-`
+import './Banner.css'
 
 function Banner({imgPaysage}) {
     return (
-        <ContainerBanner style={{backgroundImage: `url(${imgPaysage})`}}>
-            <TextBanner>Chez vous, partout et ailleurs</TextBanner>
-        </ContainerBanner>
+        <div className='ContainerBanner' style={{backgroundImage: `url(${imgPaysage})`}}>
+            <h1 className='TextBanner'>Chez vous, partout et ailleurs</h1>
+        </div>
     )
 }
 

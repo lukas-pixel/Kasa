b6b9fd3001bc58f604f1c326b589adaccf3aeb2e
marine cherdo <marine.cherdo13@gmail.com>
2023-04-24 14:33:14 +0200

    Correction du css / correction div jsx

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,22 +1,13 @@
 import './Error.css'
 import { Link } from 'react-router-dom'
-import styled from 'styled-components'
-import colors from '../../style/colors'
 import LogoError from '../../assets/404.png'
-
-const ErrorLink = styled(Link)`
-    margin-bottom: 30px;
-    color: ${colors.primary};
-`
 
 function Error() {
     return (
         <div className='ErrorContainer'>
             <img className='ErrorLogo' src={LogoError} alt="404 Logo" />
-            <div className='ContianerText'>
-                <h2 className='ErrorTxt'>Oups! La page que vous demandez n'existe pas.</h2>
-            </div>
-            <ErrorLink to='/'>Retourner sur la page d'accueil</ErrorLink>
+            <h2 className='ErrorTxt'>Oups! La page que vous demandez n'existe pas.</h2>
+            <Link className='ErrorLink' to='/'>Retourner sur la page d'accueil</Link>
         </div>
     )
 }

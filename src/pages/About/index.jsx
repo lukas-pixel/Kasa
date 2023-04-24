03690697d9d00ebcafafdb6151388cc0fcb4f2e3
marine cherdo <marine.cherdo13@gmail.com>
2023-04-24 11:59:25 +0200

    mediaQuery: remise en place du margin / mise du margin dans un fichier css

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,3 +1,4 @@
+import './About.css'
 import AboutBanner from "../../components/AboutBanner"
 import Collapse from "../../components/Collapse/index"
 import data from "../../data/About.json"
@@ -6,7 +7,7 @@ export default function Propos() {
     return (
         <div>
             <AboutBanner />
-            <div style={{margin: `0 210px`}}>
+            <div className="ContainerCollapse">
                 {data.map((about) => (
                     <Collapse key={about.id} title={about.title} content={about.description} />
                 ))}

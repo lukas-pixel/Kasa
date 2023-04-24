466d5f61551c68c14ea2429a7dd6b556496ff612
marine cherdo <marine.cherdo13@gmail.com>
2023-04-24 10:50:34 +0200

    Mise en place du media query / Creation page css

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,31 +1,10 @@
-import styled from 'styled-components'
-import colors from '../../style/colors'
-
-const Figure = styled.figure`
-    height: 340px;
-    width: 340px;
-    border-radius: 10px;
-    margin: 28px 25px 22px 25px;
-    background-position: center;
-    display: flex;
-    align-items: flex-end;
-`
-
-const TextLocation = styled.h2`
-    z-index: 1;
-    padding: 12px;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    text-decoration: none;
-    color: ${colors.White};
-`
+import './Card.css'
 
 function Card({id, cover, title}) {
     return (
-        <Figure key={id} style={{backgroundImage: `url(${cover})`}}>
-            <TextLocation>{title}</TextLocation>
-        </Figure>
+        <figure key={id} style={{backgroundImage: `url(${cover})`}}>
+            <h2 className='TextLocation'>{title}</h2>
+        </figure>
     )
 }
 

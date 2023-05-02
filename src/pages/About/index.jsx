6e6d9eb603262cc391d5b165c198b25e7f1981e9
marine cherdo <marine.cherdo13@gmail.com>
2023-05-02 10:07:37 +0200

    Correction du Probleme du Banner

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,12 +1,10 @@
 import './About.css'
-import AboutBanner from "../../components/AboutBanner"
 import Collapse from "../../components/Collapse/index"
 import data from "../../data/About.json"
 
 export default function Propos() {
     return (
         <div>
-            <AboutBanner />
             <div className="ContainerAboutContenu">
                 {data.map((about) => (
                     <Collapse key={about.id} title={about.title} content={about.description} />
